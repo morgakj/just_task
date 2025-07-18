dc11ddb50f1e2d5af73983981d854488baf25fa9
morgakj <justicemorgak21@gmail.com>
2025-07-18 11:30:52 +0100

    second commit

---
+++ task4.py
@@ -0,0 +1,37 @@
+# Arithematic operations
+#addition assignment operator
+first_number=5
+first_number+=3
+print(first_number)
+#subtraction
+second_number=6
+second_number-=10
+print(second_number)
+#multiplication
+third_number=5
+third_number*=2
+print(third_number)
+#division
+forth_number=5
+forth_number /=3
+print(forth_number)
+#floor division
+fifth_number=4
+fifth_number//=3
+print(fifth_number)
+#class task
+allawance=2000
+allawance -=400
+print(f"remaining balance is= {allawance:.2f}")
+
+allawance +=10
+print(allawance)
+allawance -=250
+print(allawance)
+percent= 25/100
+allawance *=percent
+print(percent)
+allawance -=percent
+print(f"allawance is ={allawance}")
+
+
